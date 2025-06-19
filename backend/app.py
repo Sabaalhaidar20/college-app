3f200097e013c2e6b35909e78c41dcca7d42621d
Saba Alhaidar <sabaalhaidar@my.unt.edu>
2025-06-18 21:47:09 -0500

    pushing backend code

---
+++ backend/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route("/hello")
+def hello():
+    return jsonify(message="this is a message from backend!")
