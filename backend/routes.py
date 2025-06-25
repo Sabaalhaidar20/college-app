65d538867b165abaa1ebcca77aa730a5a2e0712d
Everardo Velasquez <everardovelasquez@my.unt.edu>
2025-06-24 20:16:56 -0500

    Added route to get all interests from database

--- backend/routes.py
+++ backend/routes.py
@@ -4,16 +4,13 @@ from firebase_admin import auth
 
 routes_bp = Blueprint("routes",__name__)
 
-#@app.route("/hello")
-#def hello():
-#    return jsonify(message="this is a message from backend!")
 
 @routes_bp.route("/hello")
 def hello():
     return jsonify(message="this is a message from backend!")
 
 #create user test
-@routes_bp.route("/test", methods=["POST"])
+@routes_bp.route("/api/test", methods=["POST"])
 def test():
     try:
         test_data = {
@@ -28,8 +25,10 @@ def test():
         return jsonify({"error": str(e)}), 400
 
 
+
+
 #register/create user
-@routes_bp.route("/profiles", methods = ["POST"])
+@routes_bp.route("/api/profiles", methods = ["POST"])
 def register_user():
 
     #get and verify id token
@@ -68,3 +67,57 @@ def register_user():
     })
 
     return jsonify({"message":"Profile successfully created"}), 201
+
+
+
+
+#LOGIN route, Frontend handles actual login via firebase auth, this route simply verifies the id token and checks if user exists
+@routes_bp.route("/api/profiles",methods = ["GET"])
+def login():
+    
+    #get and verify id token
+    auth_token = request.headers.get("Authorization")       #request auth header for firebase id token
+    if not auth_token:
+        return jsonify({"error": "Authorization ID token is missing"}), 401
+    
+    if not auth_token.startswith("Bearer "):
+        return jsonify({"error":"Token has to start with Bearer (valid auth token)"}), 400
+    
+    try:
+        id_token = auth_token.split("Bearer ")[1]           #split string to extract the actual token
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']                          #the token is now the uid
+
+
+    except Exception as e:
+        return jsonify({"error":f"Invalid token: {str(e)}"}), 400
+
+    #check if the user already exists
+    user_ref = db.collection("users").document(uid)         #create a reference to a user with the uid extracted from the id token
+    if user_ref.get().exists:
+        return jsonify({"message": "User does exist"}), 200 #send frontend message confirming user exists
+    else:
+        return jsonify({"error":"User does not exist"}), 404    #user not found
+
+
+
+
+#Get all docs from the interests collection in firestore database (get all interests)
+@routes_bp.route("/api/interests", methods = ["GET"])
+def get_interests():
+
+    try:
+        interests_ref = db.collection("interests").stream()       #get all documents in the "interests" collection in firestore   
+
+        interests = []                                          #initialize empty list of interests to hold all interest dictionaries in database
+
+        for doc in interests_ref:
+            data = doc.to_dict()                           #convert firestore doc to a dictionary
+            data["id"] = doc.id                             #include the document id in dictionary as well
+
+            interests.append(data)                             #add dictionary to the list
+
+        return jsonify(interests), 200                           # return the list of interests
+
+    except Exception as e:
+        return jsonify({"error":str(e)}),500
