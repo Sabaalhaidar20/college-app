9f249a5f523f546737f9bb01bd594cdfb83f5065
Everardo Velasquez <everardovelasquez@my.unt.edu>
2025-07-23 17:02:21 -0500

    update firebase_config.py

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import firebase_admin 
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("firebase-adminkey.json")
+firebase_key = os.environ.get("FIREBASE_KEY")
+key_dict = json.loads(firebase_key)
+cred = credentials.Certificate(key_dict)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
